1b6edea453b6b8cecec4a551cc459cc7a8026637
Steven Hocking <steven.hocking@onthebeach.co.uk>
2016-12-02 14:54:25 +0000

    Add program with Autofac setup

---
+++ Service/Program.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Service.IoC;
+
+namespace Service
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = BuildContainer();
+        }
+
+        private static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<ConventionModule>();
+            return builder.Build();
+        }
+    }
+}
